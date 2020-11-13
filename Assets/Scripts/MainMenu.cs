52c9d4c3dcf468a11005ac5171c6483a83522e40
Ľubomír Jesze <lubomir.j184@gmail.com>
2020-11-13 23:08:21 +0100

    Initial commit

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        StartCoroutine(LoadScene());
+    }
+
+    IEnumerator LoadScene()
+    {
+        yield return null;
+
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync("Game");
+        asyncOperation.allowSceneActivation = false;
+        Debug.Log("Progress :" + asyncOperation.progress);
+
+        while (!asyncOperation.isDone)
+        {
+            Debug.Log("Loading progress: " + (asyncOperation.progress * 100) + "%");
+
+            if (asyncOperation.progress >= 0.9f)
+            {
+                Debug.Log("Press the space bar to continue");
+
+                if (Input.GetKeyDown(KeyCode.Space)) {
+                    asyncOperation.allowSceneActivation = true;
+                }
+            }
+
+            yield return null;
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
